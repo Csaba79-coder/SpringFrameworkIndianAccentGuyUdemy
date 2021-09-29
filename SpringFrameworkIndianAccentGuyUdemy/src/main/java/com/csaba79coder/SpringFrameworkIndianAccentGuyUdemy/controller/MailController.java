488768f301dfc19852842b29d2618972d662fb9a
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-09-29 11:13:10 +0200

    solve multiple @Component to operate properly with @Autowired using also @Qualifier annotation!

--- SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/controller/MailController.java
+++ SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/controller/MailController.java
@@ -3,6 +3,7 @@ package com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.controller;
 import com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.email.MailSender;
 // import com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.email.MockMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,31 @@ public class MailController {
 
     // needed dependency injection as in production they use SMTP mail sender, developer uses Mock ... (just for test)
     // component help for dependency injection to change smtp mail sender and mock mail sender regarding who uses it! - pair Autowired!
-    @Autowired
+    //  // for duplication of @Component needed a @Qualifier to avoid the warning message, and to make program runnable!
+    // if I use qualifier no matter if something has @Primary annotation!
+    @Qualifier("smtpMailSender")
+    @Autowired // this can be @Autowired or also a setter of it can be @Autowired! and also works with constructor!
     // private MailSender mailSender = new MockMailSender();
     private MailSender mailSender;
+
+//    public MailSender getMailSender() {
+//        return mailSender;
+//    }
+
+    // if you use both Component as annotation change the mailSender to smtp or to mock (depends which you wanna use!)
+    public void setMailSender(@Qualifier("smtpMailSender") MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+//    // This one used before the Qualifier and with the Primary annotation!
+//    public void setMailSender(MailSender smtpMailSender) {
+//        this.mailSender = smtpMailSender;
+//    }
+
+    // instead of this one use the above mentioned one!
+//    public void setMailSender(MailSender mailSender) {
+//        this.mailSender = mailSender;
+//    }
 
     @RequestMapping("/mail")
     public String mail() {
--- SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/email/MockMailSender.java
+++ SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/email/MockMailSender.java
@@ -2,21 +2,23 @@ package com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.email;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 // component help for dependency injection to change smtp mail sender and mock mail sender regarding who uses it! - pair is Autowired!
 @Component
+// @Primary // makes the injection primary! -- removing Primary annotation to try the Qualifier!
 public class MockMailSender implements MailSender {
 
     // private static final ILoggerFactory LOG = (ILoggerFactory) name -> null;
-    private static Log log = LogFactory.getLog(MockMailSender.class);
+    private static final Log LOG = LogFactory.getLog(MockMailSender.class);
 
     @Override
     public void send(String email, String subject, String body) {
         // as we are sending a mock email, all the info goes to a log!!!
 
-        log.info("Sending mock email to: " + email);
-        log.info("With subject: " + subject);
-        log.info("and body " + body);
+        LOG.info("Sending MOCK email to: " + email);
+        LOG.info("With subject: " + subject);
+        LOG.info("and body " + body);
     }
 }
