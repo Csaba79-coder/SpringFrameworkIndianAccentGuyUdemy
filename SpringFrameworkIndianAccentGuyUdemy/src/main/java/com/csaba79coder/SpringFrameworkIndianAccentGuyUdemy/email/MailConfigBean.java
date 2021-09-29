201e265a084d9bf82f85648b6d22876b7a9b3a2e
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-09-29 20:46:00 +0200

    adding profiles to Spring project dev and prod profiles! so 2 environments!

--- SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/email/MailConfigBean.java
+++ SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/email/MailConfigBean.java
@@ -2,6 +2,8 @@ package com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.email;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Configuration
@@ -9,11 +11,13 @@ import org.springframework.stereotype.Component;
 public class MailConfigBean {
 
     @Bean
+    @Profile("dev")
     public MailSender mockMailSenderBean() {
         return new MockMailSenderBean();
     }
 
     @Bean
+    @Profile("prod") // @Profile("!dev") // this is also good!
     public MailSender smtpMailSenderBean() {
         return new SmtpMailSenderBean();
     }
