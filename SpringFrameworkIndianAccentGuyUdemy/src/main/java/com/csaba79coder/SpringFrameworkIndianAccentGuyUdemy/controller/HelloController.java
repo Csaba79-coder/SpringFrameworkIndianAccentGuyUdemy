b12d05d6dc18efc1474100192b117feceb664db0
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2021-09-29 19:38:33 +0200

    adding annotations

--- SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/controller/HelloController.java
+++ SpringFrameworkIndianAccentGuyUdemy/src/main/java/com/csaba79coder/SpringFrameworkIndianAccentGuyUdemy/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.csaba79coder.SpringFrameworkIndianAccentGuyUdemy.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Value("${app.name}")
+    private String appName;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello, SpringBoot!";
+        return "Hello, SpringBoot! The name of application:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     " + appName;
     }
 
     @RequestMapping("/")
